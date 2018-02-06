ad61f1072c3258eb40133facf87fb48f556af22c
Vijay Vaidyanathan <vvijay26@gmail.com>
2018-02-06 14:17:06 -0800

    React redux app with cookies
    Simple React redux app with cookies

---
+++ src/Actions/index.js
@@ -0,0 +1,20 @@
+import { ADD_REMINDER,DELETE_REMINDER } from '../constants';
+
+export const addReminder = (text, dueDate) => {
+	const action = {
+		type: ADD_REMINDER,
+		text,
+		dueDate
+	}
+	console.log("action is ", action);
+	return action;
+}
+
+export const deleteReminder = (id) => {
+	const action = {
+		type: DELETE_REMINDER,
+		id
+	}
+	console.log("deleting in actions ", action);
+	return action;
+}
---
+++ src/component/App.jsx
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addReminder,deleteReminder } from '../Actions';
+import moment from 'moment';
+import '../App.css'
+
+class App extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			text: '',
+			dueDate: ''
+		}
+	}
+
+	addReminder(){
+		this.props.addReminder(this.state.text, this.state.dueDate);
+	}
+
+	deleteReminder(id){
+		console.log("deleting in App.jsx", id);
+		console.log('this.props',this.props);
+		this.props.deleteReminder(id);
+	}
+
+	renderReminders(){
+		const { reminders } = this.props;
+		return (
+			<ul className="list-group col-sm-4">
+			{
+				reminders.map(reminder  => {
+					return(
+						<li key={reminder.id} className="list-group-item">
+						<div className="list-item">
+						     <div>{reminder.text}</div>
+						     <div><em>{moment(new Date(reminder.dueDate)).fromNow()}</em></div>
+						</div>
+						<div 
+						className="list-item delete-button"
+						onClick={() => this.deleteReminder(reminder.id)}>
+						  &#x2715;
+						</div>
+						</li>
+					)
+			})				
+	    }
+		    </ul>
+	    )
+	}
+
+
+	render() {
+		console.log('this.props from mapStateToProps', this.state);
+		return(
+			<div className = "App">
+				<div className = "App-title">
+				   App
+				</div>
+				<div className = "form-inline reminder-form">
+					<div className = "form-group">
+						<input 
+							className = "form-control"
+							placeholder = "book "
+							onChange={event => this.setState({text:event.target.value})}
+						/>
+					</div>
+					<input
+					  className="form-control"
+					  type="datetime-local"
+					  onChange={event => this.setState({dueDate:event.target.value})}
+					/>
+					<button
+						type = "button"
+						className = "btn btn-success"
+						onClick = {() => this.addReminder()}
+					>
+					   Add	
+					</button>
+				</div>
+
+					{this.renderReminders()}
+			</div>
+		)
+
+	}
+}
+
+function mapStateToProps(state){
+	return{
+		reminders: state
+	}
+}
+
+function mapDispatchToProps(dispatch){
+	return bindActionCreators({addReminder,deleteReminder}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
