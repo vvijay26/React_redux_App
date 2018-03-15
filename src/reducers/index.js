63bab1bd628751785b481377fec600f316205f34
Vijay Vaidyanathan <vvijay26@gmail.com>
2018-03-15 12:07:20 -0700

    added some mods.
    added some detailed comments to make es6/js operators clear

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -21,6 +21,13 @@ const reminders = (state = [], action) => {
 	state = read_cookie('reminders');
 	switch(action.type){
 		case ADD_REMINDER:
+		    //The three dots (...) is a spread function of ES6 wich "Spreads" an array into different arguments
+		    //Example:- 
+		    //function sum(x, y, z) {
+            //return x + y + z;
+            //}
+            //const numbers = [1, 2, 3];
+            //console.log(sum(...numbers));
 		    reminders = [...state, reminder(action)]
 			bake_cookie('reminders',reminders);
 			return reminders;
